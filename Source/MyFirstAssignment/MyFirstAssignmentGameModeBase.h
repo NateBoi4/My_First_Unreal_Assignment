679c89d5710c04694bf557040c242edb1121a58b
Nathan Boisvert <nathan.boisvert@mymail.champlain.edu>
2022-02-01 19:04:14 -0500

    First Unreal Commit.

---
+++ Source/MyFirstAssignment/MyFirstAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyFirstAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTASSIGNMENT_API AMyFirstAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
